1178872f879bf575697adf75ea889e6b97a766ce
Georges Haidar <ghaidar0@gmail.com>
2025-11-08 13:46:06 +0000

    automatic release pr

--- .mise-tasks/bumper.sh
+++ .mise-tasks/bumper.sh
@@ -6,6 +6,6 @@ set -e
 
 export MISE_QUIET=true
 
-go build -o "$MISE_CONFIG_ROOT/local/bumper" "$MISE_CONFIG_ROOT/cmd/bumper/main.go"
+go build -mod=readonly -o "$MISE_CONFIG_ROOT/local/bumper" "$MISE_CONFIG_ROOT/cmd/bumper/main.go"
 
 exec "$MISE_CONFIG_ROOT/local/bumper" "$@"
